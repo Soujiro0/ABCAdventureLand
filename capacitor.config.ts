08e3df13153ae772041e55da78062f6c438e7758
Soujiro0 <castrudesjerry11@gmail.com>
2024-04-23 19:47:04 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Project Form Scratch',
+  webDir: 'www'
+};
+
+export default config;
