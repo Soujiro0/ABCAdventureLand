b091b09c7d7b27c643416c2ba9d4d91599783693
princessbullos <107797735+sheeeshhh31@users.noreply.github.com>
2024-06-01 04:53:36 +0800

    QUIZ 2 with overlay

--- src/app/pages/quiz/quiz-two/quiz-two.page.ts
+++ src/app/pages/quiz/quiz-two/quiz-two.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { CheckOverlayComponent } from '../../../components/check-overlay/check-overlay.component';
 
 @Component({
   selector: 'app-quiz-two',
@@ -12,103 +14,115 @@ export class QuizTwoPage implements OnInit {
       illustration: '../../../../assets/QUIZ2obj/pig.png',
       letters: { letter1: 'P', letter2: '____', letter3: 'G' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsA-inactive.png',
-        '../../../../assets/images/quiz-choices/capsE-inactive.png',
-        '../../../../assets/images/quiz-choices/capsI-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsA-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsE-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsI-inactive.png', isCorrect: true }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/bus.png',
       letters: { letter1: 'B', letter2: '____', letter3: 'S' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsU-inactive.png',
-        '../../../../assets/images/quiz-choices/capsO-inactive.png',
-        '../../../../assets/images/quiz-choices/capsA-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsU-inactive.png', isCorrect: true },
+        { imgPath: '../../../../assets/images/quiz-choices/capsO-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsA-inactive.png', isCorrect: false }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/mug.png',
       letters: { letter1: 'M', letter2: '____', letter3: 'G' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsA-inactive.png',
-        '../../../../assets/images/quiz-choices/capsU-inactive.png',
-        '../../../../assets/images/quiz-choices/capsO-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsA-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsU-inactive.png', isCorrect: true },
+        { imgPath: '../../../../assets/images/quiz-choices/capsO-inactive.png', isCorrect: false }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/axe.png',
       letters: { letter1: 'A', letter2: '____', letter3: 'E' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsY-inactive.png',
-        '../../../../assets/images/quiz-choices/capsZ-inactive.png',
-        '../../../../assets/images/quiz-choices/capsX-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsY-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsZ-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsX-inactive.png', isCorrect: true }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/ice.png',
       letters: { letter1: 'I', letter2: '____', letter3: 'E' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsL-inactive.png',
-        '../../../../assets/images/quiz-choices/capsR-inactive.png',
-        '../../../../assets/images/quiz-choices/capsC-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsL-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsR-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsC-inactive.png', isCorrect: true }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/ape.png',
       letters: { letter1: 'A', letter2: '____', letter3: 'E' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsP-inactive.png',
-        '../../../../assets/images/quiz-choices/capsZ-inactive.png',
-        '../../../../assets/images/quiz-choices/capsT-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsP-inactive.png', isCorrect: true },
+        { imgPath: '../../../../assets/images/quiz-choices/capsZ-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsT-inactive.png', isCorrect: false }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/web.png',
       letters: { letter1: 'W', letter2: '____', letter3: 'B' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsA-inactive.png',
-        '../../../../assets/images/quiz-choices/capsE-inactive.png',
-        '../../../../assets/images/quiz-choices/capsI-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsA-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsE-inactive.png', isCorrect: true },
+        { imgPath: '../../../../assets/images/quiz-choices/capsI-inactive.png', isCorrect: false }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/box.png',
       letters: { letter1: 'B', letter2: '____', letter3: 'X' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsO-inactive.png',
-        '../../../../assets/images/quiz-choices/capsE-inactive.png',
-        '../../../../assets/images/quiz-choices/capsI-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsO-inactive.png', isCorrect: true },
+        { imgPath: '../../../../assets/images/quiz-choices/capsE-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsI-inactive.png', isCorrect: false }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/owl.png',
       letters: { letter1: 'O', letter2: '____', letter3: 'W' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsL-inactive.png',
-        '../../../../assets/images/quiz-choices/capsA-inactive.png',
-        '../../../../assets/images/quiz-choices/capsW-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsL-inactive.png', isCorrect: true },
+        { imgPath: '../../../../assets/images/quiz-choices/capsA-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsW-inactive.png', isCorrect: false }
       ]
     },
     {
       illustration: '../../../../assets/QUIZ2obj/ant.png',
       letters: { letter1: 'A', letter2: '____', letter3: 'T' },
       choices: [
-        '../../../../assets/images/quiz-choices/capsL-inactive.png',
-        '../../../../assets/images/quiz-choices/capsN-inactive.png',
-        '../../../../assets/images/quiz-choices/capsF-inactive.png'
+        { imgPath: '../../../../assets/images/quiz-choices/capsL-inactive.png', isCorrect: false },
+        { imgPath: '../../../../assets/images/quiz-choices/capsN-inactive.png', isCorrect: true },
+        { imgPath: '../../../../assets/images/quiz-choices/capsF-inactive.png', isCorrect: false }
       ]
     }
   ];
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() { }
 
-  onChoiceClick() {
-    if (this.currentQuizIndex < this.quizzes.length - 1) {
-      this.currentQuizIndex++;
-    } else {
-      console.log('Quiz completed');
+  async onChoiceClick(isCorrect: boolean) {
+    const modal = await this.modalController.create({
+      component: CheckOverlayComponent,
+      cssClass: 'bottom-sheet-modal',
+      componentProps: { isCorrect },
+      showBackdrop: false,
+      backdropDismiss: false
+    });
+    await modal.present();
+    const { data } = await modal.onDidDismiss();
+
+    if (data) {
+      if (this.currentQuizIndex < this.quizzes.length - 1) {
+        this.currentQuizIndex++;
+      } else {
+        console.log('Quiz completed');
+      }
     }
   }
 }
