5f10326724cb36a059df8430f4d7ef068cffb41f
princessbullos <107797735+sheeeshhh31@users.noreply.github.com>
2024-05-31 21:26:28 +0800

    update lesson-one.ts
    img lang

--- src/app/pages/lessons/lesson-one-play/lesson-one-play.page.ts
+++ src/app/pages/lessons/lesson-one-play/lesson-one-play.page.ts
@@ -8,34 +8,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class LessonOnePlayPage implements OnInit {
   consonants: any[] = [
-    { letter: 'B', objectName: 'Ball', pronunciation: 'ball', letterImgPath: 'assets/images/consonants/B.png' },
-    { letter: 'C', objectName: 'Cat', pronunciation: 'cat', letterImgPath: 'assets/images/consonants/C.png' },
-    { letter: 'D', objectName: 'Dog', pronunciation: 'dog', letterImgPath: 'assets/images/consonants/D.png' },
-    { letter: 'F', objectName: 'Fish', pronunciation: 'fih-ish', letterImgPath: 'assets/images/consonants/F.png' },
-    { letter: 'G', objectName: 'Guitar', pronunciation: 'gih-tar', letterImgPath: 'assets/images/consonants/G.png' },
-    { letter: 'H', objectName: 'Hat', pronunciation: 'huh-t', letterImgPath: 'assets/images/consonants/H.png' },
-    { letter: 'J', objectName: 'Jellyfish', pronunciation: 'jel-ee-fish', letterImgPath: 'assets/images/consonants/J.png' },
-    { letter: 'K', objectName: 'Kite', pronunciation: 'kite', letterImgPath: 'assets/images/consonants/K.png' },
-    { letter: 'L', objectName: 'Lamp', pronunciation: 'lamp', letterImgPath: 'assets/images/consonants/L.png' },
-    { letter: 'M', objectName: 'Mouse', pronunciation: 'maws', letterImgPath: 'assets/images/consonants/M.png' },
-    { letter: 'N', objectName: 'Nest', pronunciation: 'nest', letterImgPath: 'assets/images/consonants/N.png' },
-    { letter: 'P', objectName: 'Piano', pronunciation: 'pee-an-oh', letterImgPath: 'assets/images/consonants/P.png' },
-    { letter: 'Q', objectName: 'Queen', pronunciation: 'kween', letterImgPath: 'assets/images/consonants/Q.png' },
-    { letter: 'R', objectName: 'Rainbow', pronunciation: 'rain-bohw', letterImgPath: 'assets/images/consonants/R.png' },
-    { letter: 'S', objectName: 'Sun', pronunciation: 'sun', letterImgPath: 'assets/images/consonants/S.png' },
-    { letter: 'T', objectName: 'Tiger', pronunciation: 'ty-ger', letterImgPath: 'assets/images/consonants/T.png' },
-    { letter: 'V', objectName: 'Violin', pronunciation: 'vy-oh-lin', letterImgPath: 'assets/images/consonants/V.png' },
-    { letter: 'W', objectName: 'Watch', pronunciation: 'wach', letterImgPath: 'assets/images/consonants/W.png' },
-    { letter: 'X', objectName: 'Xylophone', pronunciation: 'zy-loh-fown', letterImgPath: 'assets/images/consonants/X.png' },
-    { letter: 'Y', objectName: 'Yo-yo', pronunciation: 'yoyo', letterImgPath: 'assets/images/consonants/Y.png' },
-    { letter: 'Z', objectName: 'Zipper', pronunciation: 'zip-per', letterImgPath: 'assets/images/consonants/Z.png' }
+    { letter: 'B', objectName: 'Ball', pronunciation: 'ball', letterImgPath: '../../../../assets/lessonOnePlayObjects/ball.png' },
+    { letter: 'C', objectName: 'Cat', pronunciation: 'cat', letterImgPath: '../../../../assets/lessonOnePlayObjects/cat.png' },
+    { letter: 'D', objectName: 'Dog', pronunciation: 'dog', letterImgPath: '../../../../assets/lessonOnePlayObjects/dog.png' },
+    { letter: 'F', objectName: 'Fish', pronunciation: 'fih-ish', letterImgPath: '../../../../assets/lessonOnePlayObjects/fish.png' },
+    { letter: 'G', objectName: 'Guitar', pronunciation: 'gih-tar', letterImgPath: '../../../../assets/lessonOnePlayObjects/guitar.png' },
+    { letter: 'H', objectName: 'Hat', pronunciation: 'huh-t', letterImgPath: '../../../../assets/lessonOnePlayObjects/hat.png' },
+    { letter: 'J', objectName: 'Jellyfish', pronunciation: 'jel-ee-fish', letterImgPath: '../../../../assets/lessonOnePlayObjects/jellyfish.png' },
+    { letter: 'K', objectName: 'Kite', pronunciation: 'kite', letterImgPath: '../../../../assets/lessonOnePlayObjects/kite.png' },
+    { letter: 'L', objectName: 'Lamp', pronunciation: 'lamp', letterImgPath: '../../../../assets/lessonOnePlayObjects/lamp.png' },
+    { letter: 'M', objectName: 'Mouse', pronunciation: 'maws', letterImgPath: '../../../../assets/lessonOnePlayObjects/mouse.png' },
+    { letter: 'N', objectName: 'Nest', pronunciation: 'nest', letterImgPath: '../../../../assets/lessonOnePlayObjects/nest.png' },
+    { letter: 'P', objectName: 'Piano', pronunciation: 'pee-an-oh', letterImgPath: '../../../../assets/lessonOnePlayObjects/piano.png' },
+    { letter: 'Q', objectName: 'Queen', pronunciation: 'kween', letterImgPath: '../../../../assets/lessonOnePlayObjects/queen.png' },
+    { letter: 'R', objectName: 'Rainbow', pronunciation: 'rain-bohw', letterImgPath: '../../../../assets/lessonOnePlayObjects/rainbow.png' },
+    { letter: 'S', objectName: 'Sun', pronunciation: 'sun', letterImgPath: '../../../../assets/lessonOnePlayObjects/sun.png' },
+    { letter: 'T', objectName: 'Tiger', pronunciation: 'ty-ger', letterImgPath: '../../../../assets/lessonOnePlayObjects/tiger.png' },
+    { letter: 'V', objectName: 'Violin', pronunciation: 'vy-oh-lin', letterImgPath: '../../../../assets/lessonOnePlayObjects/violin.png' },
+    { letter: 'W', objectName: 'Watch', pronunciation: 'wach', letterImgPath: '../../../../assets/lessonOnePlayObjects/watch.png' },
+    { letter: 'X', objectName: 'Xylophone', pronunciation: 'zy-loh-fown', letterImgPath: '../../../../assets/lessonOnePlayObjects/xylophone.png' },
+    { letter: 'Y', objectName: 'Yo-yo', pronunciation: 'yoyo', letterImgPath: '../../../../assets/lessonOnePlayObjects/yoyo.png' },
+    { letter: 'Z', objectName: 'Zipper', pronunciation: 'zip-per', letterImgPath: '../../../../assets/lessonOnePlayObjects/zipper.png' }
   ];
   vowels: any[] = [
-    { letter: 'A', objectName: 'Apple', pronunciation: 'uh-pohl', letterImgPath: 'assets/images/vowels/A.png' },
-    { letter: 'E', objectName: 'Egg', pronunciation: 'eg', letterImgPath: 'assets/images/vowels/E.png' },
-    { letter: 'I', objectName: 'Igloo', pronunciation: 'ih-gloo', letterImgPath: 'assets/images/vowels/I.png' },
-    { letter: 'O', objectName: 'Orange', pronunciation: 'or-enj', letterImgPath: 'assets/images/vowels/O.png' },
-    { letter: 'U', objectName: 'Umbrella', pronunciation: 'um-brell-uh', letterImgPath: 'assets/images/vowels/U.png' }
+    { letter: 'A', objectName: 'Apple', pronunciation: 'uh-pohl', letterImgPath: '../../../../assets/lessonOnePlayObjects/apple.png' },
+    { letter: 'E', objectName: 'Egg', pronunciation: 'eg', letterImgPath: '../../../../assets/lessonOnePlayObjects/egg.png' },
+    { letter: 'I', objectName: 'Igloo', pronunciation: 'ih-gloo', letterImgPath: '../../../../assets/lessonOnePlayObjects/igloo.png' },
+    { letter: 'O', objectName: 'Orange', pronunciation: 'or-enj', letterImgPath: '../../../../assets/lessonOnePlayObjects/orange.png' },
+    { letter: 'U', objectName: 'Umbrella', pronunciation: 'um-brell-uh', letterImgPath: '../../../../assets/lessonOnePlayObjects/umbrella.png' }
   ];
   selectedLetter: any;
   letterType: string = 'consonants';
