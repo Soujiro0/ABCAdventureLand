801fdf4e07b37f076454620da10893f44e5ffabc
princessbullos <107797735+sheeeshhh31@users.noreply.github.com>
2024-05-31 20:25:26 +0800

    LESSON 1 PLAY SOUNDS

--- src/app/pages/lessons/lesson-one-play/lesson-one-play.page.ts
+++ src/app/pages/lessons/lesson-one-play/lesson-one-play.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lesson-one-play',
@@ -6,10 +7,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson-one-play.page.scss'],
 })
 export class LessonOnePlayPage implements OnInit {
+  consonants: any[] = [
+    { letter: 'B', objectName: 'Ball', pronunciation: 'ball', letterImgPath: 'assets/images/consonants/B.png' },
+    { letter: 'C', objectName: 'Cat', pronunciation: 'cat', letterImgPath: 'assets/images/consonants/C.png' },
+    { letter: 'D', objectName: 'Dog', pronunciation: 'dog', letterImgPath: 'assets/images/consonants/D.png' },
+    { letter: 'F', objectName: 'Fish', pronunciation: 'fih-ish', letterImgPath: 'assets/images/consonants/F.png' },
+    { letter: 'G', objectName: 'Guitar', pronunciation: 'gih-tar', letterImgPath: 'assets/images/consonants/G.png' },
+    { letter: 'H', objectName: 'Hat', pronunciation: 'huh-t', letterImgPath: 'assets/images/consonants/H.png' },
+    { letter: 'J', objectName: 'Jellyfish', pronunciation: 'jel-ee-fish', letterImgPath: 'assets/images/consonants/J.png' },
+    { letter: 'K', objectName: 'Kite', pronunciation: 'kite', letterImgPath: 'assets/images/consonants/K.png' },
+    { letter: 'L', objectName: 'Lamp', pronunciation: 'lamp', letterImgPath: 'assets/images/consonants/L.png' },
+    { letter: 'M', objectName: 'Mouse', pronunciation: 'maws', letterImgPath: 'assets/images/consonants/M.png' },
+    { letter: 'N', objectName: 'Nest', pronunciation: 'nest', letterImgPath: 'assets/images/consonants/N.png' },
+    { letter: 'P', objectName: 'Piano', pronunciation: 'pee-an-oh', letterImgPath: 'assets/images/consonants/P.png' },
+    { letter: 'Q', objectName: 'Queen', pronunciation: 'kween', letterImgPath: 'assets/images/consonants/Q.png' },
+    { letter: 'R', objectName: 'Rainbow', pronunciation: 'rain-bohw', letterImgPath: 'assets/images/consonants/R.png' },
+    { letter: 'S', objectName: 'Sun', pronunciation: 'sun', letterImgPath: 'assets/images/consonants/S.png' },
+    { letter: 'T', objectName: 'Tiger', pronunciation: 'ty-ger', letterImgPath: 'assets/images/consonants/T.png' },
+    { letter: 'V', objectName: 'Violin', pronunciation: 'vy-oh-lin', letterImgPath: 'assets/images/consonants/V.png' },
+    { letter: 'W', objectName: 'Watch', pronunciation: 'wach', letterImgPath: 'assets/images/consonants/W.png' },
+    { letter: 'X', objectName: 'Xylophone', pronunciation: 'zy-loh-fown', letterImgPath: 'assets/images/consonants/X.png' },
+    { letter: 'Y', objectName: 'Yo-yo', pronunciation: 'yoyo', letterImgPath: 'assets/images/consonants/Y.png' },
+    { letter: 'Z', objectName: 'Zipper', pronunciation: 'zip-per', letterImgPath: 'assets/images/consonants/Z.png' }
+  ];
+  vowels: any[] = [
+    { letter: 'A', objectName: 'Apple', pronunciation: 'uh-pohl', letterImgPath: 'assets/images/vowels/A.png' },
+    { letter: 'E', objectName: 'Egg', pronunciation: 'eg', letterImgPath: 'assets/images/vowels/E.png' },
+    { letter: 'I', objectName: 'Igloo', pronunciation: 'ih-gloo', letterImgPath: 'assets/images/vowels/I.png' },
+    { letter: 'O', objectName: 'Orange', pronunciation: 'or-enj', letterImgPath: 'assets/images/vowels/O.png' },
+    { letter: 'U', objectName: 'Umbrella', pronunciation: 'um-brell-uh', letterImgPath: 'assets/images/vowels/U.png' }
+  ];
+  selectedLetter: any;
+  letterType: string = 'consonants';
+  audio: HTMLAudioElement = new Audio();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const letter = params.get('letter')!;
+      this.letterType = params.get('type') || 'consonants';
+      this.selectedLetter = this.getLetter(letter, this.letterType);
+      this.loadAudio();
+    });
   }
 
+  getLetter(letter: string, type: string): any {
+    return type === 'vowels' ? this.vowels.find(l => l.letter === letter) : this.consonants.find(l => l.letter === letter);
+  }
+
+  getPronunciationParts(pronunciation: string | undefined): string[] {
+    return pronunciation ? pronunciation.split('-') : [];
+  }
+
+  loadAudio() {
+    this.updateAudioSource();
+  }
+
+  updateAudioSource() {
+    this.audio.src = `../../../../assets/text-to-speech/${this.selectedLetter?.letter}.mp3`;
+    this.audio.load();
+  }
+
+  playAudio() {
+    this.audio.play();
+  }
+
+  nextLetter() {
+    const letters = this.letterType === 'vowels' ? this.vowels : this.consonants;
+    const currentIndex = letters.findIndex(l => l.letter === this.selectedLetter.letter);
+    const nextIndex = (currentIndex + 1) % letters.length;
+    this.selectedLetter = letters[nextIndex];
+    this.updateAudioSource();
+  }
+
+  previousLetter() {
+    const letters = this.letterType === 'vowels' ? this.vowels : this.consonants;
+    const currentIndex = letters.findIndex(l => l.letter === this.selectedLetter.letter);
+    const previousIndex = (currentIndex - 1 + letters.length) % letters.length;
+    this.selectedLetter = letters[previousIndex];
+    this.updateAudioSource();
+  }
 }
--- src/app/pages/lessons/lesson-one/lesson-one.page.ts
+++ src/app/pages/lessons/lesson-one/lesson-one.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Letter } from 'src/app/models/letter';
 import { DataProviderService } from 'src/app/services/data-provider.service';
 
@@ -7,57 +8,57 @@ import { DataProviderService } from 'src/app/services/data-provider.service';
   templateUrl: './lesson-one.page.html',
   styleUrls: ['./lesson-one.page.scss'],
 })
-export class LessonOnePage implements OnInit{
-  vowels : Letter[] = [];
-  consonants : Letter[] = [];
+export class LessonOnePage implements OnInit {
+  vowels: Letter[] = [];
+  consonants: Letter[] = [];
   selectedSegment: string = 'vowels';
 
-  constructor(private data : DataProviderService) { }
+  constructor(private data: DataProviderService, private router: Router) { }
 
   ngOnInit(): void {
-
     this.consonants = [
-      new Letter('B', 'B' , 'b', 'assets/images/consonants/B.png', '', ''),
-      new Letter('C', 'C' , 'c', 'assets/images/consonants/C.png', '', ''),
-      new Letter('D', 'D' , 'd', 'assets/images/consonants/D.png', '', ''),
-      new Letter('F', 'F' , 'f', 'assets/images/consonants/F.png', '', ''),
-      new Letter('G', 'G' , 'g', 'assets/images/consonants/G.png', '', ''),
-      new Letter('H', 'H' , 'h', 'assets/images/consonants/H.png', '', ''),
-      new Letter('J', 'J' , 'j', 'assets/images/consonants/J.png', '', ''),
-      new Letter('K', 'K' , 'k', 'assets/images/consonants/K.png', '', ''),
-      new Letter('L', 'L' , 'l', 'assets/images/consonants/L.png', '', ''),
-      new Letter('M', 'M' , 'm', 'assets/images/consonants/M.png', '', ''),
-      new Letter('N', 'N' , 'n', 'assets/images/consonants/N.png', '', ''),
-      new Letter('P', 'P' , 'p', 'assets/images/consonants/P.png', '', ''),
-      new Letter('Q', 'Q' , 'q', 'assets/images/consonants/Q.png', '', ''),
-      new Letter('R', 'R' , 'r', 'assets/images/consonants/R.png', '', ''),
-      new Letter('S', 'S' , 's', 'assets/images/consonants/S.png', '', ''),
-      new Letter('T', 'T' , 't', 'assets/images/consonants/T.png', '', ''),
-      new Letter('V', 'V' , 'v', 'assets/images/consonants/V.png', '', ''),
-      new Letter('W', 'W' , 'w', 'assets/images/consonants/W.png', '', ''),
-      new Letter('X', 'X' , 'x', 'assets/images/consonants/X.png', '', ''),
-      new Letter('Y', 'Y' , 'y', 'assets/images/consonants/Y.png', '', ''),
-      new Letter('Z', 'Z' , 'z', 'assets/images/consonants/Z.png', '', '')
+      new Letter('B', 'Ball', 'ball', 'assets/images/consonants/B.png', '', ''),
+      new Letter('C', 'Cat', 'cat', 'assets/images/consonants/C.png', '', ''),
+      new Letter('D', 'Dog', 'dog', 'assets/images/consonants/D.png', '', ''),
+      new Letter('F', 'Fish', 'fih-ish', 'assets/images/consonants/F.png', '', ''),
+      new Letter('G', 'Guitar', 'gih-tar', 'assets/images/consonants/G.png', '', ''),
+      new Letter('H', 'Hat', 'huh-t', 'assets/images/consonants/H.png', '', ''),
+      new Letter('J', 'Jellyfish', 'jel-ee-fish', 'assets/images/consonants/J.png', '', ''),
+      new Letter('K', 'Kite', 'kite', 'assets/images/consonants/K.png', '', ''),
+      new Letter('L', 'Lamp', 'lamp', 'assets/images/consonants/L.png', '', ''),
+      new Letter('M', 'Mouse', 'maws', 'assets/images/consonants/M.png', '', ''),
+      new Letter('N', 'Nest', 'nest', 'assets/images/consonants/N.png', '', ''),
+      new Letter('P', 'Piano', 'pee-an-oh', 'assets/images/consonants/P.png', '', ''),
+      new Letter('Q', 'Queen', 'kween', 'assets/images/consonants/Q.png', '', ''),
+      new Letter('R', 'Rainbow', 'rain-bohw', 'assets/images/consonants/R.png', '', ''),
+      new Letter('S', 'Sun', 'sun', 'assets/images/consonants/S.png', '', ''),
+      new Letter('T', 'Tiger', 'ty-ger', 'assets/images/consonants/T.png', '', ''),
+      new Letter('V', 'Violin', 'vy-oh-lin', 'assets/images/consonants/V.png', '', ''),
+      new Letter('W', 'Watch', 'wach', 'assets/images/consonants/W.png', '', ''),
+      new Letter('X', 'Xylophone', 'zy-loh-fown', 'assets/images/consonants/X.png', '', ''),
+      new Letter('Y', 'Yo-yo', 'yoyo', 'assets/images/consonants/Y.png', '', ''),
+      new Letter('Z', 'Zipper', 'zip-per', 'assets/images/consonants/Z.png', '', '')
     ];
 
     this.vowels = [
-      new Letter('A', 'A' , 'a', 'assets/images/vowels/A.png', '', ''),
-      new Letter('E', 'E' , 'e', 'assets/images/vowels/E.png', '', ''),
-      new Letter('I', 'I' , 'i', 'assets/images/vowels/I.png', '', ''),
-      new Letter('O', 'O' , 'o', 'assets/images/vowels/O.png', '', ''),
-      new Letter('U', 'U' , 'u', 'assets/images/vowels/U.png', '', '')
+      new Letter('A', 'Apple', 'uh-pohl', 'assets/images/vowels/A.png', '', ''),
+      new Letter('E', 'Egg', 'eg', 'assets/images/vowels/E.png', '', ''),
+      new Letter('I', 'Igloo', 'ih-gloo', 'assets/images/vowels/I.png', '', ''),
+      new Letter('O', 'Orange', 'or-enj', 'assets/images/vowels/O.png', '', ''),
+      new Letter('U', 'Umbrella', 'um-brell-uh', 'assets/images/vowels/U.png', '', '')
     ];
   }
 
-  //This method is for geting the event value of the segment and pass it to selectedSegment variable
-  selectSegment(event : any) : void {
+  selectSegment(event: any): void {
     const selectedValue = event.detail.value;
-    this.selectedSegment = (selectedValue === 'vowels') ? 'vowels' : 'consonants';
-    this.displayContent();
+    this.selectedSegment = selectedValue;
   }
 
-  //This method returns string and controlling the display of segment
-  displayContent() : string{
-    return this.selectedSegment.toString();
+  displayContent(): string {
+    return this.selectedSegment;
+  }
+
+  openLessonOnePlay(letter: Letter): void {
+    this.router.navigate(['/lesson-one-play', { letter: letter.letter, type: this.selectedSegment }]);
   }
 }
